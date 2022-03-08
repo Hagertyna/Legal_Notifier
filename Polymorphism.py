c8d5df93a6f6b9e9a83843d18576b5a526977253
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-03-08 03:05:16 -0800

    Add files via upload
    Python polymorphism

---
+++ Polymorphism.py
@@ -0,0 +1,28 @@
+#Pholymorphism
+class iamparrot:
+    def canFly(self):
+        print("Parrot can fly...")
+    def canSwim(self):
+        print("Parrot can't swim...")
+
+class IamPenguin:
+    def canFly(self):
+        print("Penguin can't fly...")
+    def canSwim(self):
+        print("penguin can swim...")
+# common interface
+def flying_bird_test(bird):
+     bird.canFly()
+     bird.canSwim()
+#Instantiate
+#parrot class object
+bird_par = iamparrot()
+
+#penguin class object
+bird_pen = IamPenguin()
+
+#passing the object
+flying_bird_test(bird_par)
+print()
+
+flying_bird_test(bird_pen)
