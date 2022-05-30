2273872bf687ea0690541afc67a7af27319bb48c
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-05-30 06:30:03 -0700

    Create list.py

---
+++ list.py
@@ -0,0 +1,14 @@
+mixed_list = [2, "abc", 3.5, True, "Hi"]
+print(mixed_list)
+print(type(mixed_list))
+
+fruits_list = list(("banana", "cherry", "lemon"))
+print(fruits_list)
+
+type(fruits_list)
+#first two values
+print(fruits_list[0])
+print(fruits_list[1])
+#last values
+print(fruits_list[1])
+print(fruits_list[-2])
