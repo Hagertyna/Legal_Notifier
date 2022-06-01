26f63f7de148d2d6aeb7196ded0fb4ac61fdb532
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-05-31 23:32:58 -0700

    Update function.py

--- function.py
+++ function.py
@@ -1,2 +1,12 @@
 def new_function():
     print("Hi, from the new_function!")
+# call new_function()
+new_function()
+new_function()
+
+def hi_function(name):
+    print("Hi " + name)
+
+# call hi_function and give values for name
+hi_function("Fred")
+hi_function("Joe")
