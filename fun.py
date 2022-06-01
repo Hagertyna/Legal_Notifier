c1c06ce2a242e06a987bd9d84ed1d6cbb50dfeeb
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-06-01 06:36:00 -0700

    Update fun.py

--- fun.py
+++ fun.py
@@ -1,3 +1,9 @@
 def mul(x,y):
   return x *y
   
+def new_names(name1, name2, name3):
+    print("The first name is " + name1)
+    print("The second name is " + name2)
+    print("The last name is " + name3)
+    
+new_names(name1 = "ronaldo", name2 = "john", name3 = "fred")
