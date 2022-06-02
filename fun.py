1091d16246ed2bfd0acc57f2316fb9ab2e807b3a
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-06-02 04:22:58 -0700

    Update fun.py

--- fun.py
+++ fun.py
@@ -6,4 +6,10 @@ def new_names(name1, name2, name3):
     print("The second name is " + name2)
     print("The last name is " + name3)
     
-new_names(name1 = "ronaldo", name2 = "john", name3 = "fred")
+new_names(name1 = "Hagi", name2 = "Yeye", name3 = "daye")
+#defining function degree
+def degrees(x):
+    return 15 * x
+
+print(degrees(2))
+print(degrees(3))
