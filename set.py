8951aa734990eb73c7ad9c5a50e34b5926d143a2
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-06-10 06:53:09 -0700

    Create set.py

---
+++ set.py
@@ -0,0 +1,10 @@
+newset = {True, "Python", 5, 3.7}
+print(newset)
+
+#lets prnit as a set 
+
+fruits_set = set(("banana", "apple", "fig"))
+print(fruits_set)
+# each on new line
+for x in fruits_set:
+    print(x)
