bda0a27faf086e1e165c600cc0a31fc0aaf81a8c
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-06-16 06:17:41 -0700

    Update lit_coll.py

--- lit_coll.py
+++ lit_coll.py
@@ -3,3 +3,7 @@ fruits = ["fig", "lemon", "banana"] #list
 numbers = (1, 2, 3, 4) #tuple
 words = {"first word": "Banana", "Second word": "Hi"} #dict
 chars = {'A','B', 'C'} #set
+print(fruits)
+print(numbers)
+print(words)
+print(chars)
