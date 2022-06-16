da879e8b2b73a172c8445f939391d8ae9bf45404
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-06-16 06:17:19 -0700

    Create lit_coll.py

---
+++ lit_coll.py
@@ -0,0 +1,5 @@
+#literal collections
+fruits = ["fig", "lemon", "banana"] #list
+numbers = (1, 2, 3, 4) #tuple
+words = {"first word": "Banana", "Second word": "Hi"} #dict
+chars = {'A','B', 'C'} #set
