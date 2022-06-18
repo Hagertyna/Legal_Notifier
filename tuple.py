c2846a9345de961b0b2e492c9ba5dda7d17ce519
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-06-17 20:34:47 -0700

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -6,3 +6,10 @@ newfruits = ("fig", "banana", "lemon", "banana")
 print(newfruits)
 len(newfruits)
 type(newfruits)
+
+newfruit1 = ("banana")
+newfruit2 = ("banana",)
+#prints string type
+print(type(newfruit1))
+# prints tuple type
+print(type(newfruit2))
