6bec5fc0926f31cf5cfdc994bcb31f666c06857e
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-06-20 06:27:06 -0700

    Update tup.py

--- tup.py
+++ tup.py
@@ -2,3 +2,8 @@ fruits = ("apple", "banana", "fig", "cherry")
 if "banana" in fruits:
     print("Yes, banana is in fruits!")
 #part 3 of continues of tuple and tuples .py docs
+fruits_l = list(fruits)
+fruits_l[1] = "watermelon"
+fruits = tuple(fruits_l)
+
+print(fruits)
