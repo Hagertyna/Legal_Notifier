0f57faad39a38711b2b41a59382edfc9184a01f4
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-06-21 07:00:21 -0700

    Update Operators.py

--- Operators.py
+++ Operators.py
@@ -1,1 +1,11 @@
 #operators
+x = 5
+y = 10
+
+print("x + y =",x+y)
+print("x - y =",x-y)
+print("x * y =",x*y)
+print("x / y =",x/y)
+print("x % y =",x%y)
+print("x // y = ",x//y)
+print("x ** y =",x**y)
