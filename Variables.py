79085b0cb00c8d1fc1b94942ad17aa4b6cbb4651
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-06-27 22:38:40 -0700

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,1 +1,15 @@
 #python variables
+msg = "Python is Fun"
+char = "I"
+multi_line = """This is
+multiple line"""
+unicode = u"\u00dcnic\u00f6de"
+raw_string = r"raw \n string!"
+
+
+
+print(msg)
+print(char)
+print(multi_line)
+print(unicode)
+print(raw_string)
