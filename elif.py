fdc1d742c3eccfe0f311fc8b03a2447526403dce
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-07-21 23:41:02 -0700

    Update elif.py

--- elif.py
+++ elif.py
@@ -1,11 +1,11 @@
->>> x = int(input("Please enter an integer: "))
+ x = int(input("Please enter an integer: "))
 Please enter an integer: 42
->>> if x < 0:
+ if x < 0:
      x = 0
     print('Negative changed to zero')
-... elif x == 0:
-...     print('Zero')
-... elif x == 1:
-...     print('Single')
-... else:
-...     print('More')
+ elif x == 0:
+     print('Zero')
+ elif x == 1:
+     print('Single')
+ else:
+     print('More')
