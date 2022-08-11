ba30b2f1551d7c718f2f4b34ca4d7b6d2e91fcf2
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-08-10 21:39:29 -0700

    Update IF.py

--- IF.py
+++ IF.py
@@ -1,12 +1,12 @@
 
 # python program to illustrate If else 
  
-i = 20
-if (i < 15):
-    print("i is smaller than 15")
+n = 20
+if (n < 15):
+    print("n is smaller than 15")
     print("i'm in if Block")
 #executed if If statement get false
 else:
-    print("i is greater than 15")
+    print("n is greater than 15")
     print("i'm in else Block")
 print("i'm not in if and not in else Block")
