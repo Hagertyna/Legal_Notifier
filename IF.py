601e6cfb9128c29af85be3b816924c1b073ac614
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-08-10 21:38:51 -0700

    Update IF.py

--- IF.py
+++ IF.py
@@ -5,6 +5,7 @@ i = 20
 if (i < 15):
     print("i is smaller than 15")
     print("i'm in if Block")
+#executed if If statement get false
 else:
     print("i is greater than 15")
     print("i'm in else Block")
