6b09489547c9443faf0d68813b80942b6b6eb84d
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2023-02-07 07:45:19 -0800

    Update ListCompression.py

--- ListCompression.py
+++ ListCompression.py
@@ -6,7 +6,6 @@ def digitSum(n):
     return dsum
 # Initializing list
 List = [367, 111, 562, 945, 6726, 873]
- 
 # Using the function on odd elements of the list
 newList = [digitSum(i) for i in List if i & 1]
 # Displaying new list
